8e2a22b669e953ed5a558a046951e0eb0de85485
EReshetnikov <eugeny_r@inbox.ru>
2016-03-03 11:09:32 +0300

    2016-03-03

--- init.sh
+++ init.sh
@@ -1,3 +1,8 @@
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo rm /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
+
+#sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+#sudo /etc/init.d/nginx restart
+#sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
+#sudo /etc/init.d/gunicorn restart
