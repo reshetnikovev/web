6e1c61f5188b99ef4817e413ede2f7483452f70a
EReshetnikov <eugeny_r@inbox.ru>
2016-03-03 12:46:11 +0300

    2016-03-03

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
-sudo rm /etc/nginx/sites-enabled/default
+#sudo rm /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 
 #sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
