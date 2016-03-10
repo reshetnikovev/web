8a7813c86a1534f8dcf89afa0b02ee3efb2f2586
reshetnikovev <eugeny_r@inbox.ru>
2016-03-10 23:44:17 +0300

    2016-03-10

---
+++ ask/qa/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+# Create your models here.
+class Question(models.Model):
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    added_at = models.DateField()
+    rating = models.IntegerField()
+    author = models.ForeignKey(User)
+    likes = models.TextField()
+
+
+class Answer(models.Model):
+    text = models.TextField()
+    added_at = models.DateField()
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    author = models.ForeignKey(User)
